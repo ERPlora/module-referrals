adb21b09189d256d2b2eed83919c9dac88921259
ioan beilic <ioan@ioans-MacBook-Pro.local>
2026-02-24 00:24:11 +0100

    fix: remove leading indentation from module.py

--- module.py
+++ module.py
@@ -1,30 +1,30 @@
-    from django.utils.translation import gettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
-    MODULE_ID = 'referrals'
-    MODULE_NAME = _('Referral Program')
-    MODULE_VERSION = '1.0.0'
-    MODULE_ICON = 'share-social-outline'
-    MODULE_DESCRIPTION = _('Customer referral tracking and reward management')
-    MODULE_AUTHOR = 'ERPlora'
-    MODULE_CATEGORY = 'marketing'
+MODULE_ID = 'referrals'
+MODULE_NAME = _('Referral Program')
+MODULE_VERSION = '1.0.0'
+MODULE_ICON = 'share-social-outline'
+MODULE_DESCRIPTION = _('Customer referral tracking and reward management')
+MODULE_AUTHOR = 'ERPlora'
+MODULE_CATEGORY = 'marketing'
 
-    MENU = {
-        'label': _('Referral Program'),
-        'icon': 'share-social-outline',
-        'order': 61,
-    }
+MENU = {
+    'label': _('Referral Program'),
+    'icon': 'share-social-outline',
+    'order': 61,
+}
 
-    NAVIGATION = [
-        {'label': _('Dashboard'), 'icon': 'speedometer-outline', 'id': 'dashboard'},
+NAVIGATION = [
+    {'label': _('Dashboard'), 'icon': 'speedometer-outline', 'id': 'dashboard'},
 {'label': _('Referrals'), 'icon': 'share-social-outline', 'id': 'referrals'},
 {'label': _('Settings'), 'icon': 'settings-outline', 'id': 'settings'},
-    ]
+]
 
-    DEPENDENCIES = []
+DEPENDENCIES = []
 
-    PERMISSIONS = [
-        'referrals.view_referral',
+PERMISSIONS = [
+    'referrals.view_referral',
 'referrals.add_referral',
 'referrals.change_referral',
 'referrals.manage_settings',
-    ]
+]
